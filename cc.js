c75e94df1b92e4a9ad8093aa61cd7e68279ef132
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2023-10-30 09:55:21 +0800

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,6 +1,6 @@
 var rule={
     title:'厂长资源',
-    host:'https://www.czzy88.com',
+    host:'https://czz9.com',
     url:'/fyclassfyfilter',
     filterable:1,//是否启用分类筛选,
     filter_url:'{{fl.cateId}}{{fl.class}}{{fl.area}}/page/fypage',
