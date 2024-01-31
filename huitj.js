959b46903070ccce1c8c69a2aebe894f5f9eb6a8
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2024-01-31 16:00:47 +0800

    Add files via upload

---
+++ huitj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?531a844c817f9a229c87419e440275cc";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
