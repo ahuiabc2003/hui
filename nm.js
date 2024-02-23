e60c8cacdb59144cb62c427fb6f948f15f0dca37
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2024-02-23 11:29:18 +0800

    Update nm.js

--- nm.js
+++ nm.js
@@ -1,10 +1,11 @@
-
+// 发布页 https://www.nmdvd.com/
 var rule={
 	title:'农民影视',
+	//host:'https://www.nmddd.com',
 	host:'https://www.nmdvd.com/',
 	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});let src = jsp.pdfh(html,"body&&a:eq(1)&&href");print(src);HOST=src',
 	url:'/vod-list-id-fyfilter.html',
-
+	// /vod-list-id-2-pg-1-order--by-time-class-0-year-2023-letter--area--lang-.html
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}-pg-fypage-order--by-{{fl.by or "time"}}-class-0-year-{{fl.year}}-letter-{{fl.letter}}-area-{{fl.area}}-lang-',
 	filter: {
@@ -44,17 +45,17 @@ var rule={
 	},
 	搜索:'.ulPicTxt.clearfix li;*;img&&data-src;.sDes:eq(1)&&Text;*',
 
-	//是否启用辅助嗅探: 1,0
-	sniffer:1,
-	// 辅助嗅探规则js写法
-	isVideo:`js:
-		log(input);
-		if(/video\\/tos/.test(input)){
-			input = true
-		}else if(/\\.m3u8/.test(input)){
-			input = true
-		}else{
-			input = false
-		}
-	`,
+	// //是否启用辅助嗅探: 1,0
+	// sniffer:1,
+	// // 辅助嗅探规则js写法
+	// isVideo:`js:
+	// 	log(input);
+	// 	if(/video\\/tos/.test(input)){
+	// 		input = true
+	// 	}else if(/\\.m3u8/.test(input)){
+	// 		input = true
+	// 	}else{
+	// 		input = false
+	// 	}
+	// `,
 }
