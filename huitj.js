b5f818bd26cd7c5cbe1ba7d545e40022dcb658bc
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2024-02-26 15:35:43 +0800

    Update huitj.js

--- huitj.js
+++ huitj.js
@@ -1,1 +1,1 @@
-document.write('\u7f51\u9875\u7248\u5f71\u89c6\u7ad9\u7f51\u5740www.kuhui2.com <br/> <br/> <a href="http://www.kuhui2.com">\u70b9\u51fb\u8fdb\u5165</a>'); 
+document.write('\u7f51\u9875\u7248\u5f71\u89c6\u7ad9\u7f51\u5740www.kuhui2.com'); 
