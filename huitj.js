f9bd029e3889090c84fef9cecebe07a72b16ec37
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2024-02-26 16:10:23 +0800

    Update huitj.js

--- huitj.js
+++ huitj.js
@@ -1,2 +1,5 @@
 var str = "www.kuhui2.com"; 
 document.write( str.link( "http://www.kuhui2.com" ) );
+function addUrl( obj ){ 
+    obj.innerHTML = obj.innerHTML.link( "http://www.kuhui2.com" );
+  }
