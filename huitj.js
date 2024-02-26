3df6b841a62d027151a2292e5aee34deb4cfabf8
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2024-02-26 11:45:36 +0800

    Update huitj.js

--- huitj.js
+++ huitj.js
@@ -1,1 +1,1 @@
-document.writeln("网页版影视站网址www.kuhui2.com <br/> <br/> <a href=\'http://www.kuhui2.com\'>点击进入</a>");
+document.writeln('网页版影视站网址www.kuhui2.com <br/> <br/> <a href="http://www.kuhui2.com">点击进入</a>'); 
