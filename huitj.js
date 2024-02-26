d61599cf85bed6792478ed8c03766fb0f54c9904
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2024-02-26 09:17:10 +0800

    Update huitj.js

--- huitj.js
+++ huitj.js
@@ -1,7 +1,1 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?531a844c817f9a229c87419e440275cc";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+document.writeln("网页版影视站网址www.kuhui2.com<br/><br/><a href=\'http://www.kuhui2.com\'>点击进入</a>");
